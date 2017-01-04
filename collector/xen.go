550f80f9dacc2e831025f4e854f073447273d003
John Prather <john.c.prather@gmail.com>
2017-01-04 14:44:44 -0800

    Add xapi metrics gathering timeout (sets node_xapi_failure = 1)

--- collector/xen.go
+++ collector/xen.go
@@ -21,6 +21,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 
 	"context"
 
@@ -31,19 +32,25 @@ import (
 )
 
 const xapiSocket = "/var/xapi/xapi"
-const collectorPrefix = "xapi"
+const xapiPrefix = "xapi"
+const xapiTimeout = 5
 
 type xenCollector struct {
-	metrics []prometheus.Gauge
+	metrics     []prometheus.Gauge
+	boolToFloat map[bool]float64
 }
 
 func init() {
-	Factories[collectorPrefix] = NewXenCollector
+	Factories["xapi"] = NewXenCollector
 }
 
 // Take a prometheus registry and return a new Collector exposing xen data.
 func NewXenCollector() (Collector, error) {
-	return &xenCollector{}, nil
+	c := &xenCollector{}
+
+	// this comes in handy for quickly throwing together boolean metrics
+	c.boolToFloat = map[bool]float64{true: 1, false: 0}
+	return c, nil
 }
 
 // newMetric instantiates a prometheus Gauge object
@@ -52,7 +59,7 @@ func (c *xenCollector) newMetric(name string, help string,
 
 	metric := prometheus.NewGauge(prometheus.GaugeOpts{
 		Namespace:   Namespace,
-		Name:        collectorPrefix + "_" + name,
+		Name:        xapiPrefix + "_" + name,
 		Help:        help,
 		ConstLabels: labels,
 	})
@@ -60,19 +67,17 @@ func (c *xenCollector) newMetric(name string, help string,
 	return metric
 }
 
-// genXAPIMetrics pulls data from xapi and populates c.metrics
-func (c *xenCollector) genXAPIMetrics() error {
+// genXAPIMetrics pulls data from xapi and populates newMetrics
+func (c *xenCollector) genXAPIMetrics() ([]prometheus.Gauge, error) {
 
-	c.metrics = make([]prometheus.Gauge, 0)
-
-	// this comes in handy for boolean metrics below
-	boolToFloat := map[bool]float64{true: 1, false: 0}
+	// init an empty array of metrics
+	newMetrics := make([]prometheus.Gauge, 0)
 
 	var myHostRef xenAPI.HostRef
 	var iAmMaster bool
 	myHostname, err := os.Hostname()
 	if err != nil {
-		return fmt.Errorf("couldn't get my own hostname: %s", err)
+		return nil, fmt.Errorf("couldn't get my own hostname: %s", err)
 	}
 
 	xenTransport := &http.Transport{
@@ -84,55 +89,56 @@ func (c *xenCollector) genXAPIMetrics() error {
 
 	xenClient, err := xenAPI.NewClient("http://localhost/", xenTransport)
 	if err != nil {
-		return fmt.Errorf("couldn't create xapi client: %s", err.Error())
+		return nil, fmt.Errorf("couldn't create xapi client: %s", err.Error())
 	}
 
 	sessionID, err := xenClient.Session.LoginWithPassword(
 		"root", "", "1.0", "node_exporter")
 	if err != nil {
-		return fmt.Errorf("couldn't login to xapi socket: %s", err.Error())
+		return nil, fmt.Errorf("couldn't login to xapi socket: %s", err.Error())
 	}
 
 	poolRecs, err := xenClient.Pool.GetAllRecords(sessionID)
 	if err != nil {
-		return fmt.Errorf("error getting pool records: %s", err.Error())
+		return nil, fmt.Errorf("error getting pool records: %s", err.Error())
 	}
 
 	hostRecs, err := xenClient.Host.GetAllRecords(sessionID)
 	if err != nil {
-		return fmt.Errorf("error getting host records: %s\n", err.Error())
+		return nil, fmt.Errorf("error getting host records: %s\n", err.Error())
 	}
 
 	vmRecs, err := xenClient.VM.GetAllRecords(sessionID)
 	if err != nil {
-		return fmt.Errorf("error getting vm records: %s\n", err.Error())
+		return nil, fmt.Errorf("error getting vm records: %s\n", err.Error())
 	}
 
 	vmMetricsRecs, err := xenClient.VMMetrics.GetAllRecords(sessionID)
 	if err != nil {
-		return fmt.Errorf("error getting vm metrics records: %s\n", err.Error())
+		return nil, fmt.Errorf("error getting vm metrics records: %s\n", err.Error())
 	}
 
 	hostMetricsRecs, err := xenClient.HostMetrics.GetAllRecords(sessionID)
 	if err != nil {
-		return fmt.Errorf("Error getting host metrics records: %s\n", err.Error())
+		return nil, fmt.Errorf("Error getting host metrics records: %s\n", err.Error())
 	}
 
 	srRecs, err := xenClient.SR.GetAllRecords(sessionID)
 	if err != nil {
-		return fmt.Errorf("error getting sr records: %s", err)
+		return nil, fmt.Errorf("error getting sr records: %s", err)
 	}
 
 	pbdRecs, err := xenClient.PBD.GetAllRecords(sessionID)
 	if err != nil {
-		return fmt.Errorf("error getting pbd records: %s", err)
+		return nil, fmt.Errorf("error getting pbd records: %s", err)
 	}
 
 	for host, hostRec := range hostRecs {
 
 		hostMetricsRec := hostMetricsRecs[hostRec.Metrics]
 
-		hostLabels := prometheus.Labels{"host": hostRec.Hostname}
+		// any labels suitable for including in host-specific metrics
+		hostLabels := prometheus.Labels{}
 
 		// see if we can figure out our own host opaqueref
 		if hostRec.Hostname == myHostname {
@@ -161,25 +167,25 @@ func (c *xenCollector) genXAPIMetrics() error {
 		cpuCountMetric := c.newMetric("cpu_count",
 			"number of physical cpus on the host",
 			hostLabels, cpuCount)
-		c.metrics = append(c.metrics, cpuCountMetric)
+		newMetrics = append(newMetrics, cpuCountMetric)
 
 		// set cpu_allocation metric for the host
 		cpuPctAllocatedMetric := c.newMetric("cpu_pct_allocated",
 			"percent of vCPUs over physical CPUs",
 			hostLabels, float64(vCPUCount)*100/cpuCount)
-		c.metrics = append(c.metrics, cpuPctAllocatedMetric)
+		newMetrics = append(newMetrics, cpuPctAllocatedMetric)
 
 		// set memory_total metric for host
 		memoryTotalMetric := c.newMetric("memory_total",
 			"total host memory (bytes)", hostLabels,
 			float64(hostMetricsRecs[hostRec.Metrics].MemoryTotal))
-		c.metrics = append(c.metrics, memoryTotalMetric)
+		newMetrics = append(newMetrics, memoryTotalMetric)
 
 		// set memory_free metric for host
 		memoryFreeMetric := c.newMetric("memory_free",
 			"free host memory (bytes)", hostLabels,
 			float64(hostMetricsRecs[hostRec.Metrics].MemoryFree))
-		c.metrics = append(c.metrics, memoryFreeMetric)
+		newMetrics = append(newMetrics, memoryFreeMetric)
 
 		// set memory_allocation metric for host
 		memory_used := hostMetricsRec.MemoryTotal - hostMetricsRec.MemoryFree
@@ -187,19 +193,19 @@ func (c *xenCollector) genXAPIMetrics() error {
 			"percent of memory_total less memory_free over memory_total",
 			hostLabels,
 			float64(memory_used)*100/float64(hostMetricsRec.MemoryTotal))
-		c.metrics = append(c.metrics, memoryPctAllocatedMetric)
+		newMetrics = append(newMetrics, memoryPctAllocatedMetric)
 
 		// set resident_vcpu_count metric for host
 		residentVCPUCountMetric := c.newMetric("resident_vcpu_count",
 			"count of vCPUs on VMs running on the host", hostLabels,
 			float64(vCPUCount))
-		c.metrics = append(c.metrics, residentVCPUCountMetric)
+		newMetrics = append(newMetrics, residentVCPUCountMetric)
 
 		// set resident_vm_count metric for host
 		residentVMCountMetric := c.newMetric("resident_vm_count",
 			"count of VMs running on the host", hostLabels,
 			float64(vmCount))
-		c.metrics = append(c.metrics, residentVMCountMetric)
+		newMetrics = append(newMetrics, residentVMCountMetric)
 
 	}
 
@@ -226,14 +232,14 @@ func (c *xenCollector) genXAPIMetrics() error {
 		haAllowOvercommitMetric := c.newMetric("ha_allow_overcommit",
 			"if set to false then operations which would cause the pool to become "+
 				"overcommitted will be blocked", poolLabels,
-			boolToFloat[poolRec.HaAllowOvercommit])
-		c.metrics = append(c.metrics, haAllowOvercommitMetric)
+			c.boolToFloat[poolRec.HaAllowOvercommit])
+		newMetrics = append(newMetrics, haAllowOvercommitMetric)
 
 		// set ha_enabled metric for pool
 		haEnabledMetric := c.newMetric("ha_enabled",
 			"true if HA is enabled on the pool", poolLabels,
-			boolToFloat[poolRec.HaEnabled])
-		c.metrics = append(c.metrics, haEnabledMetric)
+			c.boolToFloat[poolRec.HaEnabled])
+		newMetrics = append(newMetrics, haEnabledMetric)
 
 		// set ha_host_failures_to_tolerate metric for pool
 		haHostFailuresToTolerateMetric := c.newMetric(
@@ -241,19 +247,19 @@ func (c *xenCollector) genXAPIMetrics() error {
 			"number of host failures to tolerate before the "+
 				"pool is declared to be overcommitted", poolLabels,
 			float64(poolRec.HaHostFailuresToTolerate))
-		c.metrics = append(c.metrics, haHostFailuresToTolerateMetric)
+		newMetrics = append(newMetrics, haHostFailuresToTolerateMetric)
 
 		// set the ha_overcommitted metric for pool
 		haOvercommittedMetric := c.newMetric("ha_overcommitted",
 			"true if the pool is considered to be overcommitted", poolLabels,
-			boolToFloat[poolRec.HaOvercommitted])
-		c.metrics = append(c.metrics, haOvercommittedMetric)
+			c.boolToFloat[poolRec.HaOvercommitted])
+		newMetrics = append(newMetrics, haOvercommittedMetric)
 
 		// set the wlb_enabled metric for the pool
 		wlbEnabledMetric := c.newMetric("wlb_enabled",
 			"true if workload balancing is enabled on the pool", poolLabels,
-			boolToFloat[poolRec.WlbEnabled])
-		c.metrics = append(c.metrics, wlbEnabledMetric)
+			c.boolToFloat[poolRec.WlbEnabled])
+		newMetrics = append(newMetrics, wlbEnabledMetric)
 	}
 
 	for srRef, srRec := range srRecs {
@@ -294,20 +300,20 @@ func (c *xenCollector) genXAPIMetrics() error {
 		// set the default_storage metric for the sr
 		defaultStorageMetric := c.newMetric("default_storage",
 			"true if SR is a default SR for VDIs", srLabels,
-			boolToFloat[defaultSR])
-		c.metrics = append(c.metrics, defaultStorageMetric)
+			c.boolToFloat[defaultSR])
+		newMetrics = append(newMetrics, defaultStorageMetric)
 
 		// set the physical_size metric for the sr
 		physicalSizeMetric := c.newMetric("physical_size",
 			"total physicalk size of the repository (in bytes)", srLabels,
 			float64(srRec.PhysicalSize))
-		c.metrics = append(c.metrics, physicalSizeMetric)
+		newMetrics = append(newMetrics, physicalSizeMetric)
 
 		// set the physical_utilisation metric for the sr
 		physicalUtilisationMetric := c.newMetric("physical_utilisation",
 			"physical space currently utilised on this storage repository (bytes)",
 			srLabels, float64(srRec.PhysicalUtilisation))
-		c.metrics = append(c.metrics, physicalUtilisationMetric)
+		newMetrics = append(newMetrics, physicalUtilisationMetric)
 
 		// set the physical_pct_allocated metric for the sr
 		physicalPctAllocated := float64(0)
@@ -318,37 +324,73 @@ func (c *xenCollector) genXAPIMetrics() error {
 		physicalPctAllocatedMetric := c.newMetric("physical_pct_allocated",
 			"percent of physical_utilisation over physical_size", srLabels,
 			physicalPctAllocated)
-		c.metrics = append(c.metrics, physicalPctAllocatedMetric)
+		newMetrics = append(newMetrics, physicalPctAllocatedMetric)
 
 		// set the virtual_allocation metric for the sr
 		virtualAllocationMetric := c.newMetric("virtual_allocation",
 			"sum of virtualk_sizes of all VDIs in the SR (bytes)",
 			srLabels, float64(srRec.VirtualAllocation))
-		c.metrics = append(c.metrics, virtualAllocationMetric)
+		newMetrics = append(newMetrics, virtualAllocationMetric)
 
 	}
 
-	return nil
+	return newMetrics, nil
 }
 
 func (c *xenCollector) Update(ch chan<- prometheus.Metric) (err error) {
+	// init metrics
+	var metrics []prometheus.Gauge
 
-	// if an error occurs getting data from xapi, log it and set failure boolean
-	var xapiFailure float64
-	xenErr := c.genXAPIMetrics()
-	if xenErr != nil {
-		xapiFailure = 1
+	// channels for catching results from the metrics generation go routine
+	genErrCh := make(chan error)
+	genMetricsCh := make(chan []prometheus.Gauge)
+
+	// launch generation in own routine, return results through genErrCh
+	go func(c *xenCollector, genErrCh chan error,
+		genMetricsCh chan []prometheus.Gauge) {
+
+		// attempt to generate xapi stats
+		genMetrics, genErr := c.genXAPIMetrics()
+		if genErr != nil {
+			// error occurred, send it through the error chan
+			genErrCh <- genErr
+		} else {
+			// no error, send metric array through the metrics chan
+			genMetricsCh <- genMetrics
+		}
+
+	}(c, genErrCh, genMetricsCh)
+
+	// init a failure bool to false
+	xapiFailure := false
+
+	// wait for either results or a timeout
+	select {
+
+	// handle error passed from the metrics generation go routine
+	case xenErr := <-genErrCh:
 		log.Errorln(xenErr)
+		xapiFailure = true
+
+	// handle metric array from metrics generation go routine
+	case xenMetrics := <-genMetricsCh:
+		metrics = xenMetrics
+
+	// handle timeout waiting for metrics generation
+	case <-time.After(time.Duration(xapiTimeout) * time.Second):
+		log.Errorf("xapi stats generation timeout after %d seconds", xapiTimeout)
+		xapiFailure = true
+
 	}
 
 	// set the failure metric for the xapi collector as a whole
 	xapiFailureMetric := c.newMetric("failure",
 		"boolean indicates problem with xapi interface",
-		nil, xapiFailure)
-	c.metrics = append(c.metrics, xapiFailureMetric)
+		nil, c.boolToFloat[xapiFailure])
+	metrics = append(metrics, xapiFailureMetric)
 
 	// report metrics!
-	for _, metric := range c.metrics {
+	for _, metric := range metrics {
 		metric.Collect(ch)
 	}
 	return err
